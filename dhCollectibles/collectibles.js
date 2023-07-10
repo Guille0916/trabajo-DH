ce28b1ea6acadc6d3dda5916e86f7bbc13f20a3a
Guille0916 <achoguille@gmail.com>
2023-07-10 00:48:36 -0300

    primer commit

--- dhCollectibles/collectibles.js
+++ dhCollectibles/collectibles.js
@@ -1,2 +1,20 @@
- const hotToys = require('./datos/figuras1.json');
-      console.log(hotToys);
+function importar (marca){
+     let hotToys = require("./datos/figuras1.json");
+     let bandai = require("./datos/figuras2.json");
+     let starWars = require("./datos/figuras3.json");
+     switch (marca){
+          case hotToys[0].marca:
+               return hotToys;
+          break
+          case bandai[0].marca:
+               return bandai;
+          break
+          case starWars[0].marca:
+               return starWars;
+          break
+          default:
+                return "no hay esa marca";
+
+     }
+}
+module.exports = importar;
